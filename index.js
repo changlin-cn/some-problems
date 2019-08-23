f19e91a767e467b001722f637ce8ff271acd9b23
changlin <changlin@example.com>
2019-08-23 15:26:48 +0800

    Init

---
+++ index.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: "#menu",
+  data: {
+    pages: [
+      {
+        path: "./index.html",
+        name: "home"
+      },
+      {
+        path: "./baiduIME.html",
+        name: "BaiduIME"
+      }
+    ]
+  },
+  template: `<ul>
+        <li v-for="page in pages"><a v-bind:href="page.path">{{page.name}}</li>
+    </ul>`
+});
